11998515a78f3d169241d12f71b879043d97f51b
Jordan Crawford <jordan.crawford@me.com>
2016-09-20 22:19:05 +1200

    Support location on/off checks and improve overall handling of checks. Yet to tidy these checks up further and actually start making locations requests with a location listener.

--- app/src/main/java/kiwi/jordancrawford/kiwiexplorer/MainActivity.java
+++ app/src/main/java/kiwi/jordancrawford/kiwiexplorer/MainActivity.java
@@ -1,7 +1,8 @@
 package kiwi.jordancrawford.kiwiexplorer;
 
-import android.*;
 import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -12,16 +13,27 @@ import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         ActivityCompat.OnRequestPermissionsResultCallback {
     private static final int REQUEST_LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final int REQUEST_CHECK_SETTINGS_CODE = 2;
+    private static final long LOCATION_INTERVAL = 1000;
+    private static final long FASTEST_LOCATION_INTERVAL = 1000;
 
     private GoogleApiClient googleApiClient;
     private Location lastLocation;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +48,12 @@ public class MainActivity extends AppCompatActivity implements
                     .addApi(LocationServices.API)
                     .build();
         }
+
+        // Setup a location request.
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(LOCATION_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_LOCATION_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
     }
 
     protected void onStart() {
@@ -49,14 +67,8 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void getLocation() {
-        // If don't have permission to access to the coarse location, request it.
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-            }, REQUEST_LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        }
         System.out.println("About to get location");
+        //noinspection MissingPermission
         lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
         if (lastLocation != null) {
             System.out.println("Got location");
@@ -66,12 +78,68 @@ public class MainActivity extends AppCompatActivity implements
         }
     }
 
+    // == Location permissions
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            onHaveLocationPermission();
+        } else { // If don't have permission to access to the coarse location, request it.
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, REQUEST_LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         // If the user accepted the location permission, request the location.
         if (requestCode == REQUEST_LOCATION_PERMISSION_REQUEST_CODE) {
             System.out.println("On request permission result");
             if (permissions.length == 1 && permissions[0].equals(Manifest.permission.ACCESS_COARSE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLocation();
+                onHaveLocationPermission();
+            }
+        }
+    }
+
+    // == Location settings
+    private void checkLocationSettings() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient,
+                        builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // No problems with the settings.
+                        System.out.println("Success");
+                        onAllowedToGetLocation();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Show a dialog to request the user changes the settings.
+                        try {
+                            status.startResolutionForResult(
+                                    MainActivity.this,
+                                    REQUEST_CHECK_SETTINGS_CODE);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    // If the settings change is unavailable, don't do anything.
+                }
+            }
+        });
+    }
+
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CHECK_SETTINGS_CODE) {
+            System.out.println("On check settings result");
+            if (resultCode == RESULT_OK) {
+                System.out.println("Settings are all ok");
+                onAllowedToGetLocation();
+            } else {
+                System.out.println("Settings are not ok");
             }
         }
     }
@@ -80,20 +148,23 @@ public class MainActivity extends AppCompatActivity implements
     public void onConnected(@Nullable Bundle bundle) {
         System.out.println("Connected to Google Play Services.");
 
-        // If don't have permission to access to the coarse location, request it.
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-            }, REQUEST_LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        } else {
-            getLocation();
-        }
+        checkLocationPermission();
+    }
 
+    public void onHaveLocationPermission() {
+        System.out.println("Have location permission");
+        checkLocationSettings();
+    }
+
+    public void onAllowedToGetLocation() {
+        System.out.println("Allowed to get location!");
+            // TODO: This may not always be being called when its meant to be? Having issues just after setting the location permission.
+        getLocation();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
+        // TODO?
     }
 
     @Override
